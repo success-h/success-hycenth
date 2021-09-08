2637e1296dde8882e263b65a557a8bca0f15fc25
Success Hycenth <successhycenth8@gmail.com>
2021-09-08 10:16:27 -0400

    updating site

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,25 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import GoogleFonts from 'next-google-fonts'
-import { ColorModeScript } from "@chakra-ui/react"
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ColorModeScript } from "@chakra-ui/react";
 
-export default class MyDocument extends NextDocument {
-    render() {
-        return (
-            <Html lang="en">
-                <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap" />
-                <Head />
-                <body>
-                    <ColorModeScript />
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" />
+        </Head>
+        <body>
+          <ColorModeScript />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+export default MyDocument;
