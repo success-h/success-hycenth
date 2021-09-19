2e27dcbff3648a1677a5a3f353f9ea155965bff3
Success Hycenth <successhycenth8@gmail.com>
2021-09-19 16:09:58 -0400

    updating site

--- pages/projects.js
+++ pages/projects.js
@@ -17,7 +17,7 @@ const Projects = () => {
         <title>Projects - Success Hycenth</title>
       </Head>
       <Wrapper>
-        <SimpleGrid display="grid" columns={[1, 1, 2, 2]} gap={10} mb={8}>
+        <SimpleGrid display="grid" columns={[1, 1, 2, 2]} gap={28} mb={8}>
           {WORKS_DATA.map((item) => (
             <Box
               w="300px"
@@ -26,24 +26,25 @@ const Projects = () => {
               boxShadow="md"
               boxShadow="base"
               key={item.id}
+             
             >
               <Image src={item.imageUrl} h="200px" w="100%" objectFit="cover" />
-              <Box p={5}>
-                <Text as="h1" fontSize="xl" pt={2}>
+              <Box pl={8} pr={8} pt={4} pb={4}>
+                <Text as="h1" fontSize="xl" fontWeight="bold" pt={2}>
                   {item.name}
                 </Text>
               </Box>
-              <Box pr={4} pl={4} mt={-2}>
+              <Box pr={8} pl={8} mt={-2}>
                 <Text>{item.text}</Text>
               </Box>
-              <Flex justify="space-between" m={2}>
+              <Flex justify="space-between" mt={4} mb={6} pl={6} pr={6} > 
                 <Link href={item.site}>
-                  <Button colorScheme="teal" variant="ghost" fontSize="10px">
+                  <Button color="teal.400" variant="solid" fontSize="10px" >
                     View Site
                   </Button>
                 </Link>
                 <Link href={item.source}>
-                  <Button colorScheme="teal" variant="ghost" fontSize="10px">
+                  <Button color="teal.400" variant="solid" fontSize="10px">
                     View Source
                   </Button>
                 </Link>
